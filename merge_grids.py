05b8ea70c7cc041a93df65b673c1af1d770d1cb5
gvdoord <g.vandenOord@gmail.com>
2018-09-01 12:42:56 +0200

    Testing more flexible file searching

--- merge_grids.py
+++ merge_grids.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import itertools
 import os
 import re
 import netCDF4
@@ -8,6 +9,25 @@ import numpy
 import logging
 
 log = logging.getLogger(__name__)
+
+
+def find_files(data_dir, regex_str, axes):
+    regex = re.compile(regex_str)
+    file_mapping = {}
+    for filepath in os.listdir(data_dir):
+        if re.match(regex, os.path.basename(filepath)):
+            result = re.search(regex, os.path.basename(filepath))
+            key = tuple([result.group(a) for a in axes])
+            file_mapping[key] = os.path.join(data_dir, filepath)
+    found_keys = file_mapping.keys()
+    key_values = [sorted(list(set([k[i] for k in found_keys]))) for i in range(len(axes))]
+    for key in itertools.product(*key_values):
+        if key not in found_keys:
+            numbers = tuple([str(key[axes[i] - 1]).zfill(3) for i in range(len(axes))])
+            log.error("Keys not found: %s" % str(numbers))
+            return [], {}
+    print "The key values are: ", key_values
+    return key_values, file_mapping
 
 
 # TODO: if no experiment number given, just look for all...
@@ -86,20 +106,20 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
     for name, dim in src.dimensions.items():
         dst.createDimension(name, (dst_dims[name] if not dim.isunlimited() else None))
     if any(level_list):
-        levdim =  dst.createDimension("lev", len(level_list))
-        dst_dims["lev"] =  len(levdim)
+        levdim = dst.createDimension("lev", len(level_list))
+        dst_dims["lev"] = len(levdim)
 
-    #num_steps = len(src.dimensions["time"])
+    # num_steps = len(src.dimensions["time"])
     num_steps = 100
     dt = 10
 
     # Copy variables
     if any(level_list):
-        levvar =  dst.createVariable("lev", numpy.float64, ("lev"))
+        levvar = dst.createVariable("lev", numpy.float64, ("lev"))
         levvar[:] = numpy.array(level_list)
     dst_vars, time_indices, time_slices = {}, {}, {}
     for name, variable in src.variables.items():
-        if any(level_list) and variable.dimensions != (name,): # Skip axes
+        if any(level_list) and variable.dimensions != (name,):  # Skip axes
             if variable.dimensions[0] == "time":
                 dims = ("time", "lev") + variable.dimensions[1:]
             else:
@@ -119,7 +139,6 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
                     shape.insert(1, dt)
                 else:
                     shape.insert(0, dt)
-#            print name, shape
             time_slices[name] = numpy.full(shape=shape, dtype=numpy.float64, fill_value=numpy.NaN)
 
     for i in range(0, num_steps, dt):  # loop over t
@@ -127,7 +146,7 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
         dst_vars["time"][i:iend] = src.variables["time"][i:iend]
         log.info("processing time step %d of %d...", i, num_steps)
         levs = [-1] if not any(level_list) else level_list
-        for lev in levs: # loop over levels
+        for lev in levs:  # loop over levels
             for j in dims2:  # loop over y
                 for k in dims1:  # loop over x
                     key = (k, j) if not any(level_list) else (k, j, lev)
@@ -140,11 +159,10 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
                         time_index = time_indices[varname]
                         axes = (match_dim_character(varname, vardata, 'x'), match_dim_character(varname, vardata, 'y'))
                         if time_index >= 0:
-#                            print varname, vardata.shape, key
                             values = vardata[...].take(indices=tuple(range(istart, iend)), axis=time_index)
                             if dt == 1:
-                                axes = (axes[0] - 1 if time_index < axes[0] else axes[0], axes[1] - 1 if
-                                        time_index < axes[1] else axes[1])
+                                axes = (axes[0] - 1 if time_index < axes[0] else axes[0],
+                                        axes[1] - 1 if time_index < axes[1] else axes[1])
                         else:
                             values = vardata[...]
                         if any(level_list) and "lev" in dst_vars[varname].dimensions:
@@ -158,7 +176,6 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
         for varname, vardata in time_slices.items():
             if time_indices[varname] < 0 and i > 0:
                 continue
-#            print varname, dst_vars[varname].shape, time_slices[varname].shape
             if time_indices[varname] == 0:
                 if any(level_list):
                     dst_vars[varname][istart:iend, :] = numpy.swapaxes(time_slices[varname], 0, 1)
@@ -171,7 +188,6 @@ def build_xsec(dims1, dims2, level_list, file_mapping):
 
 def copy_block(dest, src, key, xy_axes, z_axis):
     slices = []
-#    print "src shape:", src.shape, "dest shape:", dest.shape, "key:", key, "axes:", xy_axes
     for i in range(len(dest.shape)):
         if i == xy_axes[0]:
             j = i - 1 if 0 <= z_axis < i else i
@@ -192,14 +208,28 @@ def main():
     parser.add_argument("--exp", "-e", metavar="N", type=int, default=1, help="Experiment number (default: 001)")
     args = parser.parse_args()
     data_dir = args.datadir
-#    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'y', "cross")
-#    build_xsec(dims1, dims2, levs, files)
-#    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'y', 'z', "cross")
-#    build_xsec(dims1, dims2, levs, files)
-#    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'z', "cross")
-#    build_xsec(dims1, dims2, levs, files)
-    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'y', "surf_")
-    build_xsec(dims1, dims2, levs, files)
+    #    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'y', "cross")
+    #    build_xsec(dims1, dims2, levs, files)
+    #    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'y', 'z', "cross")
+    #    build_xsec(dims1, dims2, levs, files)
+    #    dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'z', "cross")
+    #    build_xsec(dims1, dims2, levs, files)
+    # dims1, dims2, levs, files = find_xsec(data_dir, args.exp, 'x', 'y', "surf_")
+    # build_xsec(dims1, dims2, levs, files)
+
+    print "Searching 2d cross sections"
+    regex = "^crossxy.x(\d+)y(\d+)." + str(args.exp).zfill(3) + ".nc$"
+    find_files(data_dir, regex, axes=(1, 2))
+    print "Searching 3d cross sections"
+    regex = "^crossxy.(\d+).x(\d+)y(\d+)." + str(args.exp).zfill(3) + ".nc$"
+    find_files(data_dir, regex, axes=(3, 1, 2))
+    print "Searching surface fields"
+    regex = "^surf_xy.x(\d+)y(\d+)." + str(args.exp).zfill(3) + ".nc$"
+    find_files(data_dir, regex, axes=(1, 2))
+    print "Searching 3d fields"
+    regex = "^fielddump.(\d+).(\d+)." + str(args.exp).zfill(3) + ".nc$"
+    find_files(data_dir, regex, axes=(1, 2))
+
 
 logging.basicConfig(level=logging.DEBUG)
 
